ba26fd1d2459d4f57a92deb4534f15354ef3de40
Brandon Cravener <BrandonCravener@users.noreply.github.com>
2022-04-15 12:25:11 -0400

    Pipes Completed

---
+++ pipes/src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  name: string = '';
+  date: string = '';
+  amount: number = 0.0; 
+  height: number = 0.0; 
+  miles: number = 0.0;
+
+
+  car = {
+    make: 'Tesla',
+    model: 'Model 3',
+    year: 2018
+  }
+
+  onNameChange(event: Event) {
+    this.name = (event.target as HTMLInputElement).value;
+  }
+
+  onDateChange(event:Event) {
+    this.date = (event.target as HTMLInputElement).value;
+  }
+
+  onAmountChange(event:Event) {
+    this.amount = parseFloat((event.target as HTMLInputElement).value);
+  }
+
+  onHeightChange(event:Event) {
+    this.height = parseFloat((event.target as HTMLInputElement).value);
+  }
+
+  onMilesChange(event:Event) {
+    this.miles = parseFloat((event.target as HTMLInputElement).value);
+  }
+
+}
